ab69d53e53f1b46e013fbebcfc2fffd54a757195
Kerenrs93 <kerenrs93@gmail.com>
2021-03-01 19:32:44 -0600

    agregar pregunta del otro lado del cuadrado

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const area=()=>{
-    let lado=prompt('Ingrese el valor del lado del cuadrado');
-    alert(lado*lado);
+    let lado1=prompt('Ingrese el valor del primer lado del cuadrado');
+    let lado2=prompt('Ingrese el valor del segundo lado del cuadrado');
+    alert(lado1*lado2);
 }
 
 
